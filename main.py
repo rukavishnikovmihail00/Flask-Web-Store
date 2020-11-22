6bdec64eed7772c9dc8eb7512e2d2fdcd0a2c02f
rukavishnikovmihail00 <rukavishnikovmihail00@yandex.ru>
2020-11-22 19:02:16 +0300

    update

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+
+from cloudipsp import Api, Checkout
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 
@@ -13,14 +18,48 @@ class Item(db.Model):
     isActive = db.Column(db.Boolean, default=True)
     # text = db.Column(db.Text, nullable=False)
 
+    def __repr__(self):
+        return self.title
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    items = Item.query.order_by(Item.price).all()
+    return render_template('index.html', data=items)
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+
+@app.route('/buy/<int:id>')
+def item_buy(id):
+    item = Item.query.get(id)
+
+    api = Api(merchant_id=1462245, secret_key='lCams2QwOYyvi2g2JS7X2NHHJAJJMCKF')
+    checkout = Checkout(api=api)
+    data = { "currency": "RUB", "amount": item.price*100 }
+    url = checkout.url(data).get('checkout_url')
+    return redirect(url)
+
+
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == "POST":
+        title = request.form['title']
+        price = request.form['price']
+
+        item = Item(title=title, price=price)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Ошибка добавления товара. Попробуйте снова"
+    else:
+        return render_template('create.html')
 
 
 
